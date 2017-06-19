a3dd31067310e1e0ed053452c27e935a0f94e577
htrucci <htrucci@gmail.com>
2017-06-19 23:03:54 +0900

    커밋되고있나

--- wordflowy_Swift/ViewController.swift
+++ wordflowy_Swift/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var myWebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        myWebView.delegate = self;
+        //loadWebPage(url: "https://workflowy.com");
+        let myHtmlBundle = Bundle.main;
+        let htmlFilePath = myHtmlBundle.path(forResource: "htmlView", ofType: "html");
+        loadWebPage(url: htmlFilePath!);
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func loadWebPage(url: String){
+        let myUrl = NSURL(string: url);
+        let myRequest = NSURLRequest(url: myUrl! as URL);
+        myWebView.loadRequest(myRequest as URLRequest);
+    }
+    
+    
 }
 
